954c268e0dadb23d05b41c801acd7dd7f73d75c5
flam1ngo <kris.s.03@mail.ru>
2024-02-17 21:15:42 +0300

    Initial commit

--- MacrosTask1/main.cpp
+++ MacrosTask1/main.cpp
@@ -1,14 +1,35 @@
-//
-//  main.cpp
-//  MacrosTask1
-//
-//  Created by Кристина Скворцова on 17.02.2024.
-//
-
 #include <iostream>
+#define MODE 2
+
+int add(int &a, int &b){
+    return a + b;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#ifdef MODE
+#if MODE == 0
+std::cout << "Работаю в режиме тренировки" << std::endl;
+
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    int a, b;
+    
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    
+    std::cout << "Результат сложения: " << add(a,b) << std::endl;
+    
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+#else
+#error "Необходимо определить MODE"
+#endif
+
     return 0;
 }
+
+
